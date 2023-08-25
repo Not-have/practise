50c7b5c0f99b31341c1bbc3ab069ff9e448e257a
LiYong <2236472089@qq.com>
2023-08-26 01:53:45 +0800

    feat: 新增了 vue demo

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <br/>
+
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-demo/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <Button label="sss" :primary="true"></Button>
+</template>
+
+<style>
+
+</style>
+<script setup lang="ts">
+import {
+  Button
+} from 'tiny-tools-vue-components'
+</script>
