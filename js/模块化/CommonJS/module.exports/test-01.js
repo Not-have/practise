976650648572fda40cd48fc614b45aa207f8952c
苟且偷安 <2236472089@qq.com>
2024-02-06 00:07:24 +0800

    feat: exports

---
+++ js/模块化/CommonJS/module.exports/test-01.js
@@ -0,0 +1,22 @@
+const obj = {
+    name: "里斯",
+    age: 16,
+    fun: function result(num1, num2) {
+        return num1 * num2;
+    }
+};
+
+setTimeout(() => {
+    obj.name = "哈哈";
+    console.log("我修改了 obj.name");
+}, 1000)
+
+setTimeout(() => {
+    console.log(obj.age);
+}, 3000)
+
+/**
+ * module 是这个模块本身的对象（test-01 test.js 这个文件的对象），这个对象里面有个属性叫 exports
+ * exports 也是对象
+ */
+module.exports = obj;
