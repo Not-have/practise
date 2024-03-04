ac4f67bf7f51c57ea9202f9c6e82b9cff84e59ca
苟且偷安 <2236472089@qq.com>
2024-03-04 11:12:29 +0800

    init

--- DemoReactnative/pages/login/style.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        paddingTop: 100
-    }
-});
