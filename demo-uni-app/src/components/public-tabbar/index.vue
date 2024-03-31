9a57b50bb984c08e4e3b1e94cd24e786f79a81d8
苟且偷安 <2236472089@qq.com>
2024-03-31 23:51:56 +0800

    uniapp - uview-plus

--- demo-uni-app/src/components/public-tabbar/index.vue
+++ demo-uni-app/src/components/public-tabbar/index.vue
@@ -1,8 +1,8 @@
 <template>
     <u-tabbar :value="1" :fixed="true" :placeholder="false" :safeAreaInsetBottom="false">
-        <u-tabbar-item text="首页" icon="home" @click="click1" />
-        <u-tabbar-item text="放映厅" icon="photo" @click="click1" />
-        <u-tabbar-item text="直播" icon="play-right" @click="click1" />
-        <u-tabbar-item text="我的" icon="account" @click="click1" />
+        <u-tabbar-item text="首页" icon="home" />
+        <u-tabbar-item text="放映厅" icon="photo" />
+        <u-tabbar-item text="直播" icon="play-right" />
+        <u-tabbar-item text="我的" icon="account" />
     </u-tabbar>
 </template>
