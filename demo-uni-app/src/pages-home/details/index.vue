3570bd548af1a749d2e5c31e997356dcefe7d5ca
苟且偷安 <2236472089@qq.com>
2024-04-03 00:08:42 +0800

    feat: params

--- demo-uni-app/src/pages-home/details/index.vue
+++ demo-uni-app/src/pages-home/details/index.vue
@@ -2,8 +2,7 @@
 import { useTestStore } from '@/store';
 import { useRouter } from '@/hooks';
 const { objTestStore } = useTestStore();
-let routes = getCurrentPages(); // 获取当前打开过的页面路由数组
-console.log(routes[routes.length - 1]);
+
 const router = useRouter();
 const handleClick = () => {
     router({
