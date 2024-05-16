3f338b7ba4044bc95687cb513d1d34e67ef1f70b
苟且偷安 <2236472089@qq.com>
2024-05-17 00:13:50 +0800

    react

--- demo-react-v18/src/App.tsx
+++ demo-react-v18/src/App.tsx
@@ -9,6 +9,7 @@ import ReducerDemo from './pages/reducer-demo';
 import CreateContextDemo from './pages/create-context-demo';
 import ContextReducerDemo from './pages/context-reducer-demo';
 import RouterApi from './pages/router-api'
+import DemoUseState from './pages/demo-use-state'
 
 function App(): JSX.Element {
     return <>
@@ -20,6 +21,8 @@ function App(): JSX.Element {
             <Link to="/ContextReducerDemo">useReducer 和 useContext 组合封装的使用</Link>
             &nbsp;
             <Link to="/RouterApi">react-router-dom 使用</Link>
+            &nbsp;
+            <Link to="/DemoUseState">useState 使用</Link>
         </nav>
         <br />
         <Routes>
@@ -27,6 +30,7 @@ function App(): JSX.Element {
             <Route path="/CreateContextDemo" element={<CreateContextDemo />} />
             <Route path="/ContextReducerDemo" element={<ContextReducerDemo />} />
             <Route path="/RouterApi" element={<RouterApi />} />
+            <Route path="/DemoUseState" element={<DemoUseState />} />
         </Routes>
     </>;
 }
