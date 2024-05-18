fed81b4f34768aacbd3c04b82fc8ab8d0abec5e0
liyong <2236472089@qq.com>
2024-05-19 01:01:04 +0800

    feat: 请求

--- demo-uni-app/src/pages-demo/demo-fetch/index.vue
+++ demo-uni-app/src/pages-demo/demo-fetch/index.vue
@@ -1,6 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { Ref, UnwrapRef } from 'vue';
+interface IObj {
+  age: string;
+}
+
+const obj: Ref<UnwrapRef<IObj>> = ref({
+  age: ''
+});
+</script>
 <template>
-  <view> 请求 </view>
+  <view>
+    请求
+    <view>
+      {{ obj?.age }}
+    </view>
+  </view>
 </template>
 
 <style scoped lang="scss"></style>
