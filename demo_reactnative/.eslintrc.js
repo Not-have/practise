d604dae0b38d233806150b82fadc72f71d27545a
苟且偷安 <2236472089@qq.com>
2024-05-24 22:38:37 +0800

    delete

--- demo_reactnative/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    root: true,
-    extends: '@react-native',
-    rules: {
-        'comma-dangle': ['error', 'never'],
-        'react/no-unstable-nested-components': ['warn', { allowAsProps: true }]
-    }
-};
