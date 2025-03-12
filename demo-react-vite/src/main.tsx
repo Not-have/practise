cc74a506f26a89d912821934f6ab4fef92429869
李勇 <yong.li@yidatec.com>
2025-03-12 11:20:09 +0800

    eslint

--- demo-react-vite/src/main.tsx
+++ demo-react-vite/src/main.tsx
@@ -7,6 +7,9 @@ import {
 
 import App from "./App.tsx";
 
+import "./index.css";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(<StrictMode>
   <App />
 </StrictMode>);
