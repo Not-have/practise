b1d38c6d5f2be012ce7f6bb8dbdf9d2aa524d4f4
李勇 <yong.li@yidatec.com>
2025-03-13 16:23:49 +0800

    feat: React.memo

--- demo-react-vite/src/pages/demo-api/components/test-use.tsx
+++ demo-react-vite/src/pages/demo-api/components/test-use.tsx
@@ -1,12 +1,42 @@
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
 import {
   ScLineText
 } from "@/rc";
-import React from "react";
+import {
+  createContext,
+  use
+} from "react";
 
-export default function TestUse() : React.ReactElement {
+const ThemeContext = createContext(null);
+
+function Container({
+  children
+}) {
+  const theme = use(ThemeContext);
+
+  return <div>
+    {theme}
+    {children}
+  </div>
+  ;
+}
+
+export default function TestUse() {
   return <>
-    <ScLineText>React.use</ScLineText>
-    <p>w</p>
-    <p>w</p>
+    <ScLineText>React.memo</ScLineText>
+
+    <dl>
+      <dt>React.use 获取 createContext 中值</dt>
+
+      <dd>
+        <ThemeContext.Provider value="dark">
+          <Container />
+        </ThemeContext.Provider>
+      </dd>
+    </dl>
+
   </>;
 }
