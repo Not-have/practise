d177d773013dac293b47137a1033fa7648ff72f3
李勇 <2236472089@qq.com>
2025-04-15 23:20:17 +0800

    feat: 更新 HomeView 组件，添加状态管理和按钮功能

--- demo-vue/src/views/HomeView.vue
+++ demo-vue/src/views/HomeView.vue
@@ -1,12 +1,40 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import {
+  useState
+} from "@mt-kit/vue-hooks";
+import {
+  watch
+} from "vue";
 
+const [state, setState] = useState({
+  age: 1
+});
+
+const handleClick = (): void => {
+  setState({
+    age: 2
+  });
+};
+
+const handleReductionClick = (): void => {
+  setState();
+};
+
+watch(() => state.age, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+});
 </script>
-
 <template>
-  <main>
-    home
-    <!--
- <TestConfigErrorHandler />
--->
-  </main>
+  <div>
+    {{ state.age }}
+    <br />
+    <br />
+    <button @click="handleClick">
+      修改
+    </button>
+    <br />
+    <button @click="handleReductionClick">
+      还原
+    </button>
+  </div>
 </template>
