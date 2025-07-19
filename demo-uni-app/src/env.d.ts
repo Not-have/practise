c030a59bd0837e5d917ec788f338f95b4e75c014
李勇 <2236472089@qq.com>
2025-07-20 03:26:13 +0800

    build(vite): 添加环境变量类型定义并调整 Vite 配置
    - 在 env.d.ts 文件中添加 ImportMetaEnv 接口，定义环境变量类型
    - 更新 vite.config.ts 文件，引入 defineConfig 和 uni 揕件
    - 调整模板代码更新逻辑，优化可读性

--- demo-uni-app/src/env.d.ts
+++ demo-uni-app/src/env.d.ts
@@ -5,3 +5,13 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+interface ImportMetaEnv {
+  readonly VITE_WE_CHAT_APP_ID: string;
+  readonly VITE_WE_CHAT_SECRET: string;
+
+  // 其他环境变量也可以在这里添加
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
