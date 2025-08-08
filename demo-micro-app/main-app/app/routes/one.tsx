43458cb56e1009839cf12bda09e2d6b11afa7c13
李勇 <2236472089@qq.com>
2025-08-09 02:47:54 +0800

    feat(demo-micro-app): integrate micro-app component in "one" route
    - Added a micro-app component to the "one" route for enhanced micro-frontend support.
    - Updated layout structure to accommodate the new child application, improving modularity and user experience.

--- demo-micro-app/main-app/app/routes/one.tsx
+++ demo-micro-app/main-app/app/routes/one.tsx
@@ -1,7 +1,15 @@
+
 export default function One() {
   return (
     <div className="w-full h-full flex-1 flex flex-col min-h-0">
-      
+      <div className="flex-1 relative">
+        {/* @ts-ignore */}
+        <micro-app
+          name="child-app"
+          url="http://localhost:5175/"
+          baseroute="/one"
+        />
+      </div>
     </div>
   );
 }
