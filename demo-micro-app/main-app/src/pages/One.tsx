6f2dbeaaf2bf7f8f6e65707ba125decde8cf3776
李勇 <yong.li@yidatec.com>
2025-08-11 17:36:32 +0800

    style(pages): 添加eslint禁用注释并格式化micro-app组件
    为One.tsx和Two.tsx中的micro-app组件添加eslint禁用注释以忽略TypeScript检查
    同时将Two.tsx中的组件属性格式化为多行以提高可读性

--- demo-micro-app/main-app/src/pages/One.tsx
+++ demo-micro-app/main-app/src/pages/One.tsx
@@ -1,4 +1,5 @@
 const One = () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   return <micro-app name='child-vue-app-02' url='http://localhost:5175/child-vue-app-02/' baseroute='/' iframe style={{ width: '100%', height: '100%' }} />
 };
--- demo-micro-app/main-app/src/pages/Two.tsx
+++ demo-micro-app/main-app/src/pages/Two.tsx
@@ -1,5 +1,13 @@
 const Two = () => {
-  return <micro-app name='child-react-app-03' url='http://localhost:5176/child-react-app-03/' baseroute='/' iframe style={{ width: '100%', height: '100%' }} />
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return <micro-app
+    name="child-react-app-03"
+    url="http://localhost:5176/child-react-app-03/"
+    baseroute="/"
+    iframe
+    style={{ width: '100%', height: '100%' }}
+  />
 };
 
 export default Two;
