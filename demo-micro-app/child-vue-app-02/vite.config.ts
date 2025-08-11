3e8f23357d6030cb6846f36553e5d97511cc637f
李勇 <2236472089@qq.com>
2025-08-11 23:41:42 +0800

    refactor(vite.config): remove build configuration for micro-app compatibility
    Eliminated the build settings in vite.config.ts that were previously set for micro-app compatibility. Updated the One.tsx component to point to the new URL for the child React app.

--- demo-micro-app/child-vue-app-02/vite.config.ts
+++ demo-micro-app/child-vue-app-02/vite.config.ts
@@ -16,26 +16,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  build: {
-    // 确保输出格式兼容 micro-app
-    target: 'es2015',
-    // 禁用代码分割，生成单个文件
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        inlineDynamicImports: true,
-      },
-    },
-    // 生成 UMD 格式，兼容性更好
-    lib: {
-      entry: 'src/main.ts',
-      name: 'VueApp',
-      formats: ['umd'],
-      fileName: () => 'app.js'
-    },
-    // 确保 CSS 内联
-    cssCodeSplit: false,
-  },
   // 开发服务器配置
   server: {
     port: 5175,
--- demo-micro-app/main-app/src/pages/One.tsx
+++ demo-micro-app/main-app/src/pages/One.tsx
@@ -1,7 +1,7 @@
 const One = () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  return <micro-app name='child-vue-app-02' url='http://localhost:5175/child-vue-app-02/' baseroute='/' iframe style={{ width: '100%', height: '100%' }} />
+  return <micro-app name='child-vue-app-02' url='http://47.108.222.61/child-react-app-03/' baseroute='/' iframe style={{ width: '100%', height: '100%' }} />
 };
 
 export default One;
