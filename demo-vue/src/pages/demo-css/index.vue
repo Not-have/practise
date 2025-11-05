e5a7e56e5eba5d316a6d88f88b02e758c69d2eee
李勇 <2236472089@qq.com>
2025-11-05 23:19:55 +0800

    fix(demo-css): 修改默认危险类型并调整样式
    将默认危险类型从'small'改为'default'以匹配实际样式
    调整属性选择器的字体大小和颜色，移除无用段落

--- demo-vue/src/pages/demo-css/index.vue
+++ demo-vue/src/pages/demo-css/index.vue
@@ -14,7 +14,7 @@ const type = [
   "small"
 ];
 
-const dangerType = ref("small");
+const dangerType = ref("default");
 
 </script>
 <template>
@@ -28,21 +28,19 @@ const dangerType = ref("small");
         {{ item }}
       </ElRadioButton>
     </ElRadioGroup>
-    <p>段落一</p>
     <p :type="dangerType">
-      段落二
+      属性选择器
     </p>
-    <p>段落三</p>
   </div>
 </template>
 <style scoped>
 p[type="large"] {
-  font-size: 24px;
+  font-size: 36px;
   color: #c9399e;
 }
 
 p[type="default"] {
-  font-size: 16px;
+  font-size: 24px;
   color: #308d50;
 }
 
