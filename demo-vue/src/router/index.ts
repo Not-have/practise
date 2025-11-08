e5c0f25b0883d78457b5ee114b7b45646fc43167
李勇 <2236472089@qq.com>
2025-11-09 00:49:55 +0800

    fix(router): 修复根路径重定向至/css

--- demo-vue/src/router/index.ts
+++ demo-vue/src/router/index.ts
@@ -6,6 +6,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      redirect: "/css"
+    },
+    {
       path: "/css",
       name: "css",
       component: () => import("@/pages/demo-css/index.vue")
