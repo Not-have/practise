4bb4c5a1298dbf4be904a71c9ad05cc72e7c4f98
李勇 <yong.li@yidatec.com>
2025-11-28 17:55:25 +0800

    feat: 初始化

---
+++ demo-react-next/pages/demo-page01/index.tsx
@@ -0,0 +1,12 @@
+import {
+  Button
+} from "antd";
+export default function DemoPage01() {
+  return (
+    <div>
+      <h1>Demo Page 01</h1>
+
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
---
+++ demo-react-next/pages/demo-page02/index.tsx
@@ -0,0 +1,7 @@
+export default function DemoPage02() {
+  return (
+    <div>
+      <h1>Demo Page 02</h1>
+    </div>
+  );
+}
